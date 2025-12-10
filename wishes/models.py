9342c1c5b1e7727053f279a87283a6ddd21fafe9
Jan Raasch <jan@janraasch.com>
2025-12-10 15:26:12 +0100

    add `Wish` model

--- wishes/models.py
+++ wishes/models.py
@@ -1,3 +1,17 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Wish(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Wishes" # Default is "Wishs" :(
+    
+    def __str__(self):
+        return f"Wish by {self.user}"
