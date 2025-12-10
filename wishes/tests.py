6af7c241564a1ed622256b985e2baf03db038f66
Jan Raasch <jan@janraasch.com>
2025-12-10 17:14:50 +0100

    add tests

--- wishes/tests.py
+++ wishes/tests.py
@@ -1,3 +1,22 @@
+from django.contrib.auth import get_user_model
+from django.shortcuts import reverse
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Wish
+
+class WishTestCase(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
+        self.client.login(username='testuser', password='testpass123')
+
+    def test_wish_show(self):
+        response = self.client.get(reverse('wishes:home'))
+        self.assertContains(response, "I want to be a Django developer")
+
+    def test_wish_update(self):
+        response = self.client.post(reverse('wishes:home'), {'text': 'I don\'t wanna be a fool!'})
+        self.assertRedirects(response, reverse('wishes:home'))
+        self.assertEqual(Wish.objects.get(user=self.user).text, 'I don\'t wanna be a fool!')
