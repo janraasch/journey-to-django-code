d52992a21e1b88ccb8b12d2667bd4188b380c093
Jan Raasch <jan@janraasch.com>
2025-12-10 15:57:21 +0100

    create `home` view

--- wishes/views.py
+++ wishes/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import Wish
+
+@login_required
+def home(request):
+    wish = Wish.objects.get_or_create(
+        user=request.user,
+        defaults={"text": "I want to be a Django developer"}
+    )[0]
+    if request.method == "POST":
+        wish.text = request.POST.get("text", "")
+        wish.save()
+        return redirect('wishes:home')
+    return render(request, 'wishes/home.html', {'wish': wish})
